970123ada82780469c052c3516ce5f3f5fa929f6
Kayra <KayraAlat@gmail.com>
2018-10-25 17:59:45 +0100

    Added convinience functions for model

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -8,6 +11,12 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def age(self):
+        return timezone.now() - self.pub_date
 
 
 class Choice(models.Model):
