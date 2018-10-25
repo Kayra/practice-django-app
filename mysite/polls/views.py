f57dee59c112588e244726e23477cd00f2f47896
Kayra <KayraAlat@gmail.com>
2018-10-25 21:27:16 +0100

    Added detail dynamic render method

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
 from polls.models import Question, Choice
@@ -8,15 +8,25 @@ def index(request):
 
     latest_question_list = Question.objects.order_by('pub_date')[:5]
     polls_index_template = 'polls/index.html'
-    context = {
+    template_context = {
         'latest_question_list': latest_question_list
     }
 
-    return render(request, polls_index_template, context)
+    return render(request, polls_index_template, template_context)
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}")
+
+    polls_detail_template = 'polls/detail.html'
+
+    try:
+        template_context = {
+            'question': Question.objects.get(pk=question_id)
+        }
+    except:
+        raise Http404('Question does not exist')
+
+    return render(request, polls_detail_template, template_context)
 
 
 def results(request, question_id):
