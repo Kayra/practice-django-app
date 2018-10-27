88f8b2d784c152e8402f47f98751d9781e01a1d2
Kayra <KayraAlat@gmail.com>
2018-10-27 10:47:13 +0100

    Vote view bug fix

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 from polls.models import Question, Choice
 
