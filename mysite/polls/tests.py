e89724dc4ab8f51b9164f302fc994a63590e4cbc
Kayra <KayraAlat@gmail.com>
2018-11-05 09:55:00 +0000

    Added more tests for polls model

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -18,3 +18,25 @@ class QuestionModelTests(TestCase):
         future_question = Question(pub_date=future_time)
 
         self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+
+        """
+        was_published_recently() returns True for questions with pub_date within the last day
+        """
+
+        within_last_day = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=within_last_day)
+
+        self.assertIs(recent_question.was_published_recently(), True)
+
+    def test_was_published_recently_with_old_question(self):
+
+        """
+        was_published_recently() returns True for questions with pub_date older than one day
+        """
+
+        older_than_one_day = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=older_than_one_day)
+
+        self.assertIs(old_question.was_published_recently(), False)
