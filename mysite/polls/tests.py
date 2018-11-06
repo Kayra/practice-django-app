54edbd303ac3f2102235fa07c650efd4f25e68b5
Kayra <KayraAlat@gmail.com>
2018-11-06 07:46:47 +0000

    Added detail view test

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -110,6 +110,18 @@ class QuestionDetailViewTests(TestCase):
         self.assertEqual(response.status_code, 404)
 
 
+    def test_past_question(self):
+
+        """
+        The detail view of a question with a pub_date in the past will display it and it's text
+        """
+
+        past_question = create_question(question_text="Past question", days=30)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+
+        self.assertContains(response, past_question.question_text)
+        
 
 class QuestionModelTests(TestCase):
 
