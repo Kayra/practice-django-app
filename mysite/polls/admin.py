ec7802a61fae0ffd3a09ef75108b372c54d06261
Kayra <KayraAlat@gmail.com>
2018-11-08 19:11:26 +0000

    Updated polls admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from polls.models import Question, Choice
 
 
-admin.site.register([Question, Choice])
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
