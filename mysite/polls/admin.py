b8de03f524d774f13df62f317073c463c3a5cfb4
Kayra <KayraAlat@gmail.com>
2018-11-10 19:19:10 +0000

    Updated polls admin to have choices directly under related Question

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,11 +3,17 @@ from django.contrib import admin
 from polls.models import Question, Choice
 
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 1
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,                  {'fields': ['question_text']}),
         ('Date information',    {'fields': ['pub_date']})
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
